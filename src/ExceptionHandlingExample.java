d619894db058228a4acfd62f465bf800c90f591e
John Berkley <jberkley95@gmail.com>
2018-10-01 15:20:28 -0700

    Lambda Basics 17 Code

---
+++ src/ExceptionHandlingExample.java
@@ -0,0 +1,27 @@
+import java.util.function.BiConsumer;
+
+/**
+ * @author John Berkley
+ * CPP Class: <Insert Class Here>
+ * Date Created: Oct 01, 2018
+ */
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        int[] someNumbers = {1, 2, 3, 4};
+        int key = 0;
+
+        process(someNumbers, key, (v, k) -> {
+            try {
+                System.out.println(v / k);
+            } catch (ArithmeticException e) {
+                System.out.println("An arithmetic exception happened!");
+            }
+        });
+    }
+
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
+        for (int i : someNumbers) {
+            consumer.accept(i, key);
+        }
+    }
+}
