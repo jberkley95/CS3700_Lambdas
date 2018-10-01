6291f9a2df80b7c8e10a47652a88e4eb85fc4030
John Berkley <jberkley95@gmail.com>
2018-10-01 15:05:09 -0700

    Lambda Basics 14 Code

--- src/Unit1ExerciseSolutionJava8.java
+++ src/Unit1ExerciseSolutionJava8.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author John Berkley
@@ -6,4 +9,36 @@ package PACKAGE_NAME;
  * Date Created: Oct 01, 2018
  */
 public class Unit1ExerciseSolutionJava8 {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Caroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+
+        // Step 1: Sort list by last name
+        Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+
+        // Step 2: Create a method that prints all elements in the list
+        printConditionally(people, p -> true);
+
+        // Step 3: Create a method that prints all people that have last name beginning with C
+        System.out.println("Printing all persons with the last name beginning with C");
+        printConditionally(people, p -> p.getFirstName().startsWith("C"));
+
+        System.out.println("Printing all persons with the first name beginning with C");
+        printConditionally(people, p -> p.getLastName().startsWith("C"));
+    }
+
+    private static void printConditionally(List<Person> people, Condition condition) {
+        for (Person p : people) {
+            if (condition.test(p)) {
+                System.out.println(p);
+            }
+        }
+    }
 }
+
+
