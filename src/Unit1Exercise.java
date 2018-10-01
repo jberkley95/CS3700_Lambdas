ec7d37b0316413734ef4a9a93b007d1b0c921ef5
John Berkley <jberkley95@gmail.com>
2018-10-01 14:43:40 -0700

    Lambda Basics 13 Code

---
+++ src/Unit1Exercise.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author John Berkley
+ * CPP Class: <Insert Class Here>
+ * Date Created: Oct 01, 2018
+ */
+public class Unit1Exercise {
+    List<Person> people = Arrays.asList(
+            new Person("Charles", "Dickens", 60),
+            new Person("Lewis", "Caroll", 42),
+            new Person("Thomas", "Carlyle", 51),
+            new Person("Charlotte", "Bronte", 45),
+            new Person("Matthew", "Arnold", 39)
+    );
+
+    // Step 1: Sort list by last name
+
+    // Step 2: Create a method that prints all elements in the list
+
+    // Step 3: Create a method that prints all people that have last name beginning with C
+}
