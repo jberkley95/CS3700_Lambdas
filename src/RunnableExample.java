c806a3aa9abaf4626e384b21a11804826e0b17af
John Berkley <jberkley95@gmail.com>
2018-10-01 14:19:57 -0700

    Lambda Basics 11 Code

---
+++ src/RunnableExample.java
@@ -0,0 +1,21 @@
+/**
+ * @author John Berkley
+ * CPP Class: <Insert Class Here>
+ * Date Created: Oct 01, 2018
+ */
+public class RunnableExample {
+    public static void main(String[] args) {
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printed inside Runnable");
+            }
+        });
+
+        myThread.run();
+
+        Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside Runnable"));
+        myLambdaThread.run();
+
+    }
+}
